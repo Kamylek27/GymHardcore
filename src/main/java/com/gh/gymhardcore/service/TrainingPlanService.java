10e330ce99321dba5bc9508d3cd31cfe9dcc7a32
Kamil <kamylekdeveloper@gmail.com>
2022-07-31 14:30:27 +0200

    fixed null repository

--- src/main/java/com/gh/gymhardcore/service/TrainingPlanService.java
+++ src/main/java/com/gh/gymhardcore/service/TrainingPlanService.java
@@ -4,15 +4,15 @@ import com.gh.gymhardcore.entity.TrainingPlan;
 import com.gh.gymhardcore.repository.TrainingPlanRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class TrainingPlanService {
-    private TrainingPlanRepository trainingPlanRepository;
+    private final TrainingPlanRepository trainingPlanRepository;
 
     public List<TrainingPlan> getAllTrainingPlan() {
         return trainingPlanRepository.findAll();
