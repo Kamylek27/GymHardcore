93e680b66568ef47af78f857be52e241738ac895
Kamil Cieślak <kcieslak@oakfusion.pl>
2022-11-06 22:19:11 +0100

    done autofill data

--- src/main/java/com/gh/gymhardcore/configuration/LoadData.java
+++ src/main/java/com/gh/gymhardcore/configuration/LoadData.java
@@ -6,7 +6,6 @@ import com.gh.gymhardcore.enums.TypeTraining;
 import com.gh.gymhardcore.repository.TrainingRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
@@ -17,9 +16,8 @@ import java.util.List;
 public class LoadData {
     private final TrainingRepository trainingRepository;
     private boolean switcher = false;
-
     private static final Logger log = LoggerFactory.getLogger(LoadData.class);
-    List<Double> listPress = new ArrayList<>() {{
+    final List<Double> listPress = new ArrayList<>() {{
         add(20.0);
         add(27.5);
         add(32.5);
@@ -29,34 +27,95 @@ public class LoadData {
 
     }};
 
+    final List<Double> listSquat = new ArrayList<>() {{
+        add(40.0);
+        add(50.0);
+        add(60.0);
+        add(65.0);
+        add(75.0);
+        add(85.0);
+
+    }};
+
+    final List<Double> listBenchPress = new ArrayList<>() {{
+        add(40.0);
+        add(50.0);
+        add(60.0);
+        add(65.0);
+        add(75.0);
+        add(85.0);
+
+    }};
+
+    final List<Double> listDeadLift = new ArrayList<>() {{
+        add(47.5);
+        add(60.0);
+        add(70.0);
+        add(77.5);
+        add(87.5);
+        add(100.0);
+
+    }};
+
     public LoadData(TrainingRepository trainingRepository) {
         this.trainingRepository = trainingRepository;
     }
 
-    public void changeSwitcher() {
+    public boolean changeSwitcher() {
         if (!switcher) {
+            fillPressDatabase();
             switcher = true;
         } else {
-            switcher = false;
+            log.info("Data already filled");
         }
-        fillDatabase();
+        return switcher;
     }
 
 
-    public void fillDatabase() {
+    public void fillPressDatabase() {
 
-        if (switcher) {
-            MainExercise press = new MainExercise();
+        MainExercise press = new MainExercise();
+        MainExercise squat = new MainExercise();
+        MainExercise benchPress = new MainExercise();
+        MainExercise deadLift = new MainExercise();
 
-            press.setLocalDate(LocalDate.now());
-            press.setTypeTraining(TypeTraining.A);
-            press.setMainExercises(MainExercises.Press);
-            press.setOneRm(58);
-            press.setTM(52.5);
-            press.setWeight(listPress);
+        press.setLocalDate(LocalDate.now());
+        press.setTypeTraining(TypeTraining.A);
+        press.setMainExercises(MainExercises.Press);
+        press.setOneRm(58);
+        press.setTM(52.5);
+        press.setWeight(listPress);
 
-            trainingRepository.save(press);
-        }
+        squat.setLocalDate(LocalDate.now());
+        squat.setTypeTraining(TypeTraining.B);
+        squat.setMainExercises(MainExercises.Squat);
+        squat.setOneRm(110);
+        squat.setTM(100);
+        squat.setWeight(listSquat);
+
+        benchPress.setLocalDate(LocalDate.now());
+        benchPress.setTypeTraining(TypeTraining.A);
+        benchPress.setMainExercises(MainExercises.BenchPress);
+        benchPress.setOneRm(110);
+        benchPress.setTM(100);
+        benchPress.setWeight(listBenchPress);
+
+        deadLift.setLocalDate(LocalDate.now());
+        deadLift.setTypeTraining(TypeTraining.B);
+        deadLift.setMainExercises(MainExercises.DeadLift);
+        deadLift.setOneRm(130);
+        deadLift.setTM(117.5);
+        deadLift.setWeight(listDeadLift);
+
+        trainingRepository.save(press);
+        trainingRepository.save(squat);
+        trainingRepository.save(benchPress);
+        trainingRepository.save(deadLift);
+    }
+
+    public void deleteFillData() {
+        trainingRepository.deleteAll();
+        switcher = false;
     }
 }
 
--- src/main/java/com/gh/gymhardcore/controller/FillController.java
+++ src/main/java/com/gh/gymhardcore/controller/FillController.java
@@ -3,6 +3,7 @@ package com.gh.gymhardcore.controller;
 
 import com.gh.gymhardcore.configuration.LoadData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,14 @@ public class FillController {
     }
 
     @PostMapping("/switch")
-    public void switcher() {
-        loadData.changeSwitcher();
+    public ResponseEntity<?> switcher() {
+        return ResponseEntity.ok("Data added");
     }
+
+    @PostMapping("/delete")
+    public ResponseEntity<?> deleteFillData() {
+        loadData.deleteFillData();
+        return ResponseEntity.ok("Fill deleted");
+    }
+
 }
